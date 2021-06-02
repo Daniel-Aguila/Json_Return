13a9d8e9245ee09f0c469db9b884dd69ea2c06d0
Daniel-Aguila <danielbaguila@gmail.com>
2021-06-01 19:32:52 -0500

    search function is finished

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from urllib.request import urlopen
 import re
+import collections
 
 def saveData(json_url):
     json_object = urlopen(json_url) #gets the json object from the http request
@@ -28,15 +29,55 @@ def index():
         "error": "error string"
     }
 
-    json_object = json.dumps(json_write)
+    #create a json string and then a json object
+    #json loads returns an object from a json representing string
+    json_string = json.dumps(json_write)
+    json_object = json.loads(json_string)
     file.close()
 
     return json_object
 
+def search(string_):
+    json_data = index()
+    wanted_word = string_.upper()
+    count_of_word_per_sentence = 0
+
+    dict = {}
+    count = 0
+    for sentence in json_data['data']:
+        myList = []
+        count+=1
+        for word in sentence.split():
+            #remove special characters so for example "dinner." is now "dinner"
+            word = re.sub('[^A-Za-z0-9]+', '', word).upper()
+            if(wanted_word == word):
+                count_of_word_per_sentence += 1
+        myList.append(count_of_word_per_sentence)
+        count_of_word_per_sentence = 0
+        dict[sentence] = myList
+
+        sorted_dict = sorted(dict.items(),key=lambda x:x[1], reverse=True)
+
+    data = []
+    for key, value in sorted_dict:
+        data.append(key)
+    json_write = {
+        "results": data,
+        "error": "error string"
+    }
+
+    #create a json string and then a json object
+    #json loads returns an object from a json representing string
+    json_string = json.dumps(json_write)
+    json_object = json.loads(json_string)
+
+    return json_object
 
 def main():
     saveData("https://gist.githubusercontent.com/marshyski/d5839816c2ea730185b0af3570cbc2f7/raw/d6aebfc3f0c202b17fe6b27aae023297d9ba6a67/sentences.json")
-    index()
-
+    print(search("we"))
+    print(search("In"))
+    print(search("dinner"))
+    print(search("Dinner"))
 if __name__ == "__main__":
     main()
